ee841e4558c3d433ba2a714d06da2e57e32daa4e
zhangzhen <1094705507@qq.com>
2018-10-10 00:01:06 +0800

    新增canvas

--- src/component/canvas/index.js
+++ src/component/canvas/index.js
@@ -16,11 +16,107 @@ export default class Map extends React.Component{
     }
 
     componentDidMount() {
-        let canvas1 = document.getElementById('canvas1');
-        if(canvas1.getContext) {
-            let ctx = canvas1.getContext('2d');
-            console.log(ctx);
-        }
+        this.drawClock();
+    }
+
+    drawClock() {
+        let clock = function() {
+            let canvas = document.getElementById('canvas1');
+            let PI = Math.PI;
+            let now = new Date();
+            let hour = now.getHours();
+            hour = hour > 12 ? hour - 12 : hour;
+            let minutes = now.getMinutes();
+            let seconds = now.getSeconds();
+            if (canvas.getContext) {
+                let ctx = canvas.getContext('2d');
+                ctx.save();
+                ctx.clearRect(0, 0, 150, 150);
+                // 画小时刻度
+                ctx.translate(75, 75);
+                ctx.scale(0.4, 0.4);
+                ctx.rotate(-PI / 2);
+                ctx.fillStyle = 'red';
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 8;
+                ctx.lineCap = 'round';
+                for (let i = 0; i < 12; i++) {
+                    ctx.beginPath();
+                    ctx.moveTo(100, 0);
+                    ctx.lineTo(120, 0);
+                    ctx.stroke();
+                    ctx.rotate(PI / 6);
+                    ctx.closePath();
+                }
+                ctx.save();
+                ctx.restore();
+
+                // 画分钟刻度
+                ctx.lineWidth = 5;
+                for (let i = 0; i < 60; i++) {
+                    if (i % 5 != 0) {
+                        ctx.beginPath();
+                        ctx.moveTo(115, 0);
+                        ctx.lineTo(120, 0);
+                        ctx.stroke();
+                    }
+                    ctx.rotate(PI / 30);
+                    ctx.closePath();
+                }
+                ctx.save();
+
+                ctx.restore();
+                // 画时针
+                ctx.beginPath();
+                ctx.lineWidth = 12;
+                ctx.strokeStyle = 'red';
+                ctx.rotate(PI * hour / 6 + PI * minutes / 360 + PI * seconds / 21600);
+                ctx.moveTo(-20, 0);
+                ctx.lineTo(60, 0);
+                ctx.stroke();
+                ctx.save();
+
+                ctx.restore();
+                // 画分针
+                ctx.rotate(PI * minutes / 30 + PI * seconds / 1800);
+                ctx.lineWidth = 8;
+                ctx.strokeStyle = 'blue';
+                ctx.beginPath();
+                ctx.moveTo(-28, 0);
+                ctx.lineTo(75, 0);
+                ctx.stroke();
+                ctx.save();
+
+                ctx.restore();
+                // 画秒针
+                ctx.rotate(PI * seconds / 30);
+                ctx.lineWidth = 4;
+                ctx.strokeStyle = 'orange';
+                ctx.beginPath();
+                ctx.moveTo(-36, 0);
+                ctx.lineTo(90, 0);
+                ctx.stroke();
+                ctx.save();
+
+                // 画时针，分针，秒针交叉点的圆
+                ctx.beginPath();
+                ctx.arc(0, 0, 15, 0, PI * 2);
+                ctx.fill();
+                ctx.beginPath();
+                ctx.lineWidth = 5;
+                ctx.arc(95, 0, 10, 0, PI * 2);
+                ctx.stroke();
+                ctx.restore();
+                ctx.beginPath();
+                ctx.strokeStyle = 'black';
+                ctx.lineWidth = 10;
+                ctx.arc(0, 0, 125, 0, PI * 2);
+                ctx.stroke();
+                ctx.restore();
+            }
+            window.requestAnimationFrame(clock);
+        };
+        window.requestAnimationFrame(clock);
     }
 
 
